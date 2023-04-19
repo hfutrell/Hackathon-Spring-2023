45fb1b4f478a1a16e58f2f7c1f2aaf38a6461f89
hfutrell <holmes@gingerlabs.com>
2023-04-19 12:50:04 -0700

    Sample Metal project

---
+++ Hackathon macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Hackathon macOS
+//
+//  Created by Holmes Futrell on 4/19/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
